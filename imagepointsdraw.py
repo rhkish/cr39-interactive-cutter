650ee8cb277ac36d7774bf1e5e4a6fab29a5a83c
unknown <rekish@mit.edu>
2023-07-26 13:28:28 -0400

    seems functional now

---
+++ imagepointsdraw.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib
+import numpy.ma as ma
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+
+def addPoint(scat, new_point, c='C0'):
+    old_off = scat.get_offsets()
+    new_off = np.concatenate([old_off,np.array(new_point, ndmin=2)])
+    old_c = scat.get_facecolors()
+    new_c = np.concatenate([old_c, np.array(matplotlib.colors.to_rgba(c), ndmin=2)])
+
+    scat.set_offsets(new_off)
+    scat.set_facecolors(new_c)
+
+    scat.axes.figure.canvas.draw_idle()
+
+def removePoint(scat):
+    off = scat.get_offsets()[:-1]
+    c = scat.get_facecolors()[:-1]
+
+    scat.set_offsets(off)
+    scat.set_facecolors(c)
+
+    scat.axes.figure.canvas.draw_idle()
+    
+
+
+class ImagePointsDraw:
+
+    def __init__(self,):
+        self.x_points= []
+        self.y_points= []
+
+    def onClickPoint(self, event,):
+
+        if event.button == 1:
+            self.x_points.append(event.xdata)
+            self.y_points.append(event.ydata)
+            addPoint(self.scat, (event.xdata, event.ydata))
+            self.line.set_data(self.x_points, self.y_points)
+            self.line.figure.canvas.draw()
+
+        elif event.button == 3:
+            
+            self.deleteLastSelection()
+            
+
+    def drawImage(self, data, **imargs):
+        fig, ax = plt.subplots()
+        fig.canvas.mpl_connect('button_press_event', self.onClickPoint)
+        #im = ax.imshow(data, alpha = .8, **imargs)
+        dedges = np.arange(0, 30, 0.5)
+        cedges = np.arange(-0.5, 100.5, 1)
+        hist, dedges, cedges, im = ax.hist2d(data[0,:], data[1,:], bins = (dedges, cedges))
+        ax.cla()
+        dmesh = dedges[0:59] + 0.25 
+        cmesh = cedges[0:100] + 0.5
+        contours = ax.contour(dmesh, cmesh, hist.T, levels = 100, colors = 'black', **imargs)
+        ax.set_title("Press left mouse to select area\n Press right mouse to remove last selected point\n Select points CCW")
+        self.scat = ax.scatter(self.x_points, self.y_points)
+        self.line, = ax.plot([0], [0])
+
+    def drawContours(self, data, edges0, edges1, **imargs):
+        fig, ax = plt.subplots()
+        fig.canvas.mpl_connect('button_press_event', self.onClickPoint)
+        #im = ax.imshow(data, alpha = .8, **imargs)
+        hist, dedges, cedges, im = ax.hist2d(data[0,:], data[1,:], bins = (edges0, edges1))
+        ax.cla()
+        mesh0 = edges0[0:-1] + 0.25 
+        mesh1 = edges1[0:-1] + 0.5
+        contours = ax.contour(mesh0, mesh1, hist.T, levels = 20, colors = 'black', **imargs)
+        ax.set_title("Press left mouse to select area\n Press right mouse to remove last selected point\n Select points CCW")
+        self.scat = ax.scatter(self.x_points, self.y_points)
+        self.line, = ax.plot([0], [0])
+
+            
+
+    def deleteLastSelection(self,):
+
+        removePoint(self.scat)
+        try:
+            self.x_points.pop()
+            self.y_points.pop()
+            self.line.set_data(self.x_points, self.y_points)
+            self.line.figure.canvas.draw()
+        except:
+            print("Cannot remove points when there are none!")
+            
+
+    def getSelectedPolygon(self,):
+        vertices = [Point(point) for point in zip(self.x_points, self.y_points)]
+        return Polygon(vertices)
+
+        
+
+        
+
+
--- interactive_shape_cutter.py
+++ interactive_shape_cutter.py
@@ -8,7 +8,6 @@ import matplotlib.pyplot as plt
 import pickle
 import glob
 import sys
-sys.path.insert(1, "../Select-Arbitrary-Shapes-in-Images-main")
 #import reconstruction 
 from imagepointsdraw import ImagePointsDraw
 from shapely.geometry import Point
@@ -26,32 +25,48 @@ cpsa_path = Path("C:\\", "Users", "Reece", "Desktop", "PRAD UROP stuff", "test.c
 scan = Scan.from_cpsa(cpsa_path)
 #scan.to_hdf5(Path(os.path.dirname(os.path.realpath(__file__)), "temp.hdf5")) #might not work
 dc_array = scan.trackdata[:,2:4]
-print(dc_array)
+print(dc_array[:,0])
 
 
 #Plot and select in CD space
 print("Select the desired region")
 draw_obj = ImagePointsDraw()
 imargs = {
-    'extent' : [0, 100,
+    'extent' : [0, 30,
                 0, 100],
     'origin' : 'lower',
-    'vmin' : 0, 'vmax' : np.max(dc_array)
 }
-draw_obj.drawImage(dc_array.T, **imargs)
+dedges = np.arange(0, 30, 0.5)
+cedges = np.arange(-0.5, 100.5, 1)
+
+draw_obj.drawContours(dc_array.T, dedges, cedges, **imargs)
 plt.xlabel("diameter")
 plt.ylabel("contrast")
 plt.show()
 
 poly = draw_obj.getSelectedPolygon()
 
-bool_mask = np.zeros(Bfield.T.shape, dtype=np.bool_)
+bool_mask = np.zeros(dc_array.shape, dtype=np.bool_)
+plt.cla()
 
-for i in range(bool_mask.shape[0]):
-    for j in range(bool_mask.shape[1]):
-        bool_mask[i][j] = poly.contains(Point(x[i][j], y[i][j]))
-
+for i in range(len(dc_array)):
+    bool_mask[i][0] = poly.contains(Point(dc_array[i][0], dc_array[i][1]))
+    bool_mask[i][1] = bool_mask[i][0]
 
 
 #Select corresponding points and show plot of xy space
+xy_array = scan.trackdata[:, 0:2]
+masked_xy_array = ma.MaskedArray(xy_array, mask = ~bool_mask)
+kept_xy_array = masked_xy_array[~masked_xy_array.mask] #1D list of all non-masked values
+kept_xy_array = np.reshape(kept_xy_array, (-1, 2))
+print(kept_xy_array)
+print(kept_xy_array.shape)
+plt.hist2d(kept_xy_array[:,0],kept_xy_array[:,1], bins = (100,100), range = [[-5, 5],[-5,5]]) 
+plt.title("xy histogram")
+plt.xlabel("x (um)")
+plt.ylabel("y (um)")
+plt.colorbar()
+plt.show()
+
+
 
