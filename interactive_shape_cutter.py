ccb0da3519dc8ec61a5271d1b564704e91d29df6
unknown <rekish@mit.edu>
2023-07-27 14:56:59 -0400

    cut early to optimize speed

--- interactive_shape_cutter.py
+++ interactive_shape_cutter.py
@@ -19,9 +19,22 @@ cpsa_path = Path("C:\\", "Users", "Reece", "Desktop", "PRAD UROP stuff", "test.c
 
 scan = Scan.from_cpsa(cpsa_path)
 #scan.to_hdf5(Path(os.path.dirname(os.path.realpath(__file__)), "temp.hdf5")) #might not work
-dc_array = scan.trackdata[:,2:4]
-print(dc_array[:,0])
+darray = scan.trackdata[:,2]
+carray = scan.trackdata[:,3]
+xarray = scan.trackdata[:, 0]
+yarray = scan.trackdata[:, 1]
 
+#alter as needed
+dmax = 30
+dmin = 0
+cmax = 5
+cmin = 0
+
+good_indices = np.logical_and(np.logical_and(darray > dmin, darray < dmax), np.logical_and(carray > cmin, carray < cmax))
+darray = darray[good_indices]
+carray = carray[good_indices]
+xarray = xarray[good_indices]
+yarray = yarray[good_indices]
 
 #Plot and select in CD space
 print("Select the desired region")
@@ -32,30 +45,27 @@ imargs = {
     'origin' : 'lower',
 }
 dedges = np.arange(0, 30, 0.5)
-cedges = np.arange(-0.5, 100.5, 1)
+cedges = np.arange(-0.5, 30.5, 1)
 
-draw_obj.drawContours(dc_array.T, dedges, cedges, **imargs)
+draw_obj.drawContours(darray, carray, dedges, cedges, **imargs)
 plt.xlabel("diameter")
 plt.ylabel("contrast")
 plt.show()
 
 poly = draw_obj.getSelectedPolygon()
 
-bool_mask = np.zeros(dc_array.shape, dtype=np.bool_)
+bool_mask = np.zeros(darray.shape, dtype=np.bool_)
 plt.cla()
 
-for i in range(len(dc_array)):
-    bool_mask[i][0] = poly.contains(Point(dc_array[i][0], dc_array[i][1]))
-    bool_mask[i][1] = bool_mask[i][0]
+for i in range(len(darray)):
+    bool_mask[i] = poly.contains(Point(darray[i], carray[i]))
 
 
 #Select corresponding points and show plot of xy space
-xy_array = scan.trackdata[:, 0:2]
-masked_xy_array = ma.MaskedArray(xy_array, mask = ~bool_mask)
-kept_xy_array = masked_xy_array[~masked_xy_array.mask] #1D list of all non-masked values
-kept_xy_array = np.reshape(kept_xy_array, (-1, 2))
+yarray = yarray[bool_mask]
+xarray = xarray[bool_mask]
 
-counts, xedges, yedges, im = plt.hist2d(kept_xy_array[:,0],kept_xy_array[:,1], bins = (100,100), range = [[-5, 5],[-5,5]]) 
+counts, xedges, yedges, im = plt.hist2d(xarray, yarray, bins = (100,100), range = [[-5, 5],[-5,5]]) 
 plt.title("xy histogram")
 plt.xlabel("x (um)")
 plt.ylabel("y (um)")
